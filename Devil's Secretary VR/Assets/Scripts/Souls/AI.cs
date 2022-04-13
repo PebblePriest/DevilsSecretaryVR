6afb2c24e88f10bbf8b59ebf3e3b1b39c54dd87f
PebblePriest <rkdsway@hotmail.com>
2022-04-13 14:24:48 -0600

    Changed Enemy AI

--- Devil's Secretary VR/Assets/Scripts/Souls/AI.cs
+++ Devil's Secretary VR/Assets/Scripts/Souls/AI.cs
@@ -8,29 +8,60 @@ using UnityEngine.UI;
 
 public class AI : MonoBehaviour
 {
+    [Header("Skull Controls")]
+    [Tooltip("Variables to find certain values of the player position")]
+    public LayerMask thePlayer;
+    public Transform player;
+
+    [Tooltip("Variables used to determine the skull and its animations")]
     public NavMeshAgent agent;
     public Animator anim;
-    public Transform player;
-    [Range(0, 100)] public float speed;
-    [Range(1, 500)] public float walkRadius;
+    private Rigidbody rb;
+    //Prefab used to spawn the dead object after the skull has been slain
+    public GameObject deadSkull;
+
+    [Tooltip("Booleans used to determine the location of the player, and if the skull can change states in order to pursue or attack.")]
+    public float sightRange = 10, attackRange = 3;
+    public Collider[] playerInSight, playerInAttack; 
+    public bool canWalk, playerInSRange = false, playerInARange = false;
+
+    [Tooltip("Variables used to determine how often the skull can attack and how much damage is done")]
+    //Timer variable to be set to determine how quickly the skull can attack with its fireball
+    private float timeBetweenAttacks = 1.5f;
+    bool hasAttacked;
+
+    [Tooltip("Variables used to determine the attack power and health of the skull")]
     public Image healthBar;
     public float Health;
     private float maxHealth = 100f;
-    public GameObject deadSkull;
     private float weaponDamage = 1f;
+    public GameObject projectile;
+
+    [Tooltip("Variables used to determine the speed and height of the Skull")]
+    public float height;
+    [Range(0, 100)] public float speed;
+    [Range(1, 500)] public float walkRadius;
+
+    [Tooltip("Variables to determine the distance of the Wander State")]
+    public float wanderRadius;
+    public float wanderTimer;
+    private float timer;
+
+    
 
     public void Awake()
     {
-       
+        agent = GetComponent<NavMeshAgent>();
+        rb = GetComponent<Rigidbody>();
+        player = GameObject.FindWithTag("Player").transform;
+        timer = wanderTimer;
     }
     void Start()
     {
-        player = GameObject.FindWithTag("Player").transform;
-        agent = GetComponent<NavMeshAgent>();
+       
         if (agent != null)
         {
             agent.speed = speed;
-            agent.SetDestination(RandomNavMeshLocation());
         }
         anim = this.GetComponent<Animator>();
         Health = maxHealth;
@@ -39,24 +70,67 @@ public class AI : MonoBehaviour
     }
     void Update()
     {
-        if (agent != null && agent.remainingDistance <= agent.stoppingDistance)
+        CheckCircles();
+        if (!playerInSRange && !playerInARange)
         {
-            agent.SetDestination(RandomNavMeshLocation());
+            Debug.Log("is Idling");
+            canWalk = true;
+            timer += Time.deltaTime;
+            if (timer >= wanderTimer)
+            {
+                //Finds a new point on the nav mesh, sets the location, then restarts the timer to find another point
+                agent.speed = .5f;
+                Vector3 newPos = RandomNavSphere(transform.position, wanderRadius, -1);
+                agent.SetDestination(newPos);
+                timer = 0;
+            }
         }
+            
+        
+        if (playerInSRange && !playerInARange)
+        {
+            Debug.Log("Is running");
+            canWalk = true;
+            timer += Time.deltaTime;
+            if (timer >= wanderTimer)
+            {
+                //Finds a new point on the nav mesh, sets the location, then restarts the timer to find another point
+                agent.speed = 2f;
+                Vector3 newPos = RandomNavSphere(transform.position, wanderRadius, -1);
+                agent.SetDestination(newPos);
+                timer = 0;
+
+                playerInSight = null;
+                playerInSRange = false;
+            }
+        }
+        if(playerInSRange && playerInARange)
+        {
+            Debug.Log("Is Attacking");
+            canWalk = false;
+            agent.speed = 0f;
+            Attack();
+        }
+        
+
+        //Change the y location of the skull to above the ground by this much.
+        transform.position = new Vector3(transform.position.x, transform.position.y + height, transform.position.z);
         healthBar.fillAmount = Health / maxHealth;
 
     }
-    public Vector3 RandomNavMeshLocation()
+    public static Vector3 RandomNavSphere(Vector3 origin, float dist, int layermask)
     {
-        Vector3 finalPosition = Vector3.zero;
-        Vector3 randomPosition = Random.insideUnitSphere * walkRadius;
-        randomPosition += transform.position;
-        if (NavMesh.SamplePosition(randomPosition, out NavMeshHit hit, walkRadius, 1))
-        {
-            finalPosition = hit.position;
-        }
-        return finalPosition;
+        Vector3 randDirection = Random.insideUnitSphere * dist;
+
+        randDirection += origin;
+
+        NavMeshHit navHit;
+
+        NavMesh.SamplePosition(randDirection, out navHit, dist, layermask);
+
+        return navHit.position;
     }
+    
     public void TakeDamage(float amnt)
     {
         Debug.Log(Health);
@@ -71,6 +145,36 @@ public class AI : MonoBehaviour
             Instantiate(deadSkull, location, rotate);
         }
     }
+    public void Attack()
+    {
+        if (!canWalk)
+        {
+            agent.SetDestination(transform.position);
+            transform.LookAt(player);
+            if (!hasAttacked)
+            {
+               Rigidbody rigb = Instantiate(projectile, transform.position, Quaternion.identity).GetComponent<Rigidbody>();
+
+                rigb.AddForce(transform.forward * 20f, ForceMode.Impulse);
+                rigb.AddForce(transform.up * 5f, ForceMode.Impulse);
+
+                hasAttacked = true;
+                Invoke(nameof(ResetAttacks), timeBetweenAttacks);
+            }
+        }
+    }
+    public void ResetAttacks()
+    {
+        Debug.Log("ResetAttack");
+        hasAttacked = true;
+        canWalk = true;
+        playerInSight = null;
+        playerInAttack = null;
+        playerInSRange = false;
+        playerInARange = false;
+        timer = 0;
+
+    }
     private void OnTriggerStay(Collider other)
     {
         if (other.CompareTag("lightning"))
@@ -80,7 +184,28 @@ public class AI : MonoBehaviour
         }
 
     }
+    public void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, attackRange);
+        Gizmos.DrawWireSphere(transform.position, sightRange);
+    }
+    public void CheckCircles()
+    {
+        playerInSight = Physics.OverlapSphere(transform.position, sightRange, thePlayer);
 
+        if (playerInSight.Length > 0)
+        {
+            playerInSRange = true;
+        }
+
+        playerInAttack = Physics.OverlapSphere(transform.position, attackRange, thePlayer);
+
+        if(playerInAttack.Length > 0)
+        {
+            playerInARange = true;
+        }
+    }
 
 
 }
